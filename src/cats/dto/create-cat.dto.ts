18a1499f93121c3a06e7d5eafb699b4938bd5026
AFSAL A AZEEZ <afsalaazeez@gmail.com>
2025-04-27 15:41:48 +0530

    added swagger

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,12 +1,16 @@
 import { IsString, IsInt, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCatDto {
+  @ApiProperty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsInt()
   age: number;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   breed?: string;
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,20 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // 👈 import Swagger
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 👉 Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Cats Example') // Title of API
+    .setDescription('The Cats API description') // Description
+    .setVersion('1.0') // Version
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // serve at /api
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new AllExceptionsFilter()); // 👈 Apply custom error filter
